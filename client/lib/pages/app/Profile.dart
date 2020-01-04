66faee633afc7c1cf6fe77c85e356652749b84d0
daehruoydeef <daeh@tuta.io>
2020-01-04 21:00:40 +0100

    backup before migrating to androidX

--- client/lib/pages/app/Profile.dart
+++ client/lib/pages/app/Profile.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
+import '../settings/./Allgemein.dart';
+import '../settings/Support.dart';
+import '../settings/UeberUns.dart';
+import '../settings/Datenschutz.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 class Profile extends StatefulWidget {
   Profile({Key key}) : super(key: key);
@@ -15,11 +20,35 @@ Widget _header(context) {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 20.0, // has the effect of softening the shadow
+            spreadRadius: 1.0, // has the effect of extending the shadow
+            offset: Offset(
+              5.0, // horizontal, move right 10
+              5.0, // vertical, move down 10
+            ),
+          )
+        ],
       ),
       width: MediaQuery.of(context).size.width * 0.9,
       height: 150,
       child: Row(
-        children: <Widget>[Text("hello"), Text("name")],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+              height: 80,
+              width: 80,
+              child: Image.asset('assets/images/profile.png')),
+          Container(
+            width: 200,
+            child: Text(
+              "Ricardo Joseph",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
       ),
     ),
   );
@@ -44,30 +73,68 @@ Widget _settings(context) {
             ),
           ),
           _settingsButton(
-            "profile",
+            "Profile",
+            FeatherIcons.settings,
+            Allgemein(),
             context,
           ),
-          _settingsButton("profile", context),
-          _settingsButton("profile", context),
+          _settingsButton(
+              "Privacy", FeatherIcons.shield, Datenschutz(), context),
+          _settingsButton("Support", FeatherIcons.lifeBuoy, Support(), context),
+          _settingsButton("About", FeatherIcons.info, About(), context),
         ],
       ));
 }
 
-Widget _settingsButton(route, context) {
+Widget _settingsButton(route, icon, pressed, context) {
   // if button is pressed it will navigate to the route specified ion the params
 
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: FlatButton(
-        onPressed: () => print("go to"),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => (Allgemein())));
+        },
         child: Container(
-          height: 75,
-          width: MediaQuery.of(context).size.width * 0.9,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white,
-          ),
-        )),
+            height: 75,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 20.0, // has the effect of softening the shadow
+                  spreadRadius: 1.0, // has the effect of extending the shadow
+                  offset: Offset(
+                    5.0, // horizontal, move right 10
+                    5.0, // vertical, move down 10
+                  ),
+                )
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.lightBlue[100]),
+                    child: Icon(icon)),
+                Container(
+                  width: 200,
+                  child: Text(route, style: TextStyle(fontSize: 16)),
+                ),
+                Container(
+                  child: Icon(FeatherIcons.chevronRight),
+                )
+              ],
+            ))),
   );
 }
 
@@ -76,7 +143,11 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[100],
-        appBar: AppBar(title: Text("Profile")),
+        appBar: AppBar(
+          title: Text("Profile"),
+          backgroundColor: Colors.grey[100],
+          elevation: 0,
+        ),
         body: Center(
             child: Column(children: <Widget>[
           _header(context),
