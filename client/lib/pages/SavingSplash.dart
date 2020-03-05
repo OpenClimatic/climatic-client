bc17529bc9748c2345c32468e9b822762f5c73cd
Oskar <linux-os@linux.fritz.box>
2020-03-05 12:22:24 +0100

    added simple navigation for quick actions

---
+++ client/lib/pages/SavingSplash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SavingSplash extends StatelessWidget {
+  const SavingSplash({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+        child: Text("You are great!"),
+      ),
+    );
+  }
+}
